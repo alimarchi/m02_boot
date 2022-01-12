b860a8cfd921ea5a95ea68eb75d4abb02e9e74b3
Alice <marchi_alice@yahoo.com>
2022-01-12 19:42:58 +0100

    Tortugas

---
+++ objetos.py
@@ -0,0 +1,11 @@
+import turtle
+
+tortugita = turtle.Turtle()
+otraTortugita = turtle.Turtle()
+
+tortugita.shape("turtle")
+tortugita.fd(50)
+
+otraTortugita.color("green")
+otraTortugita.left(90)
+otraTortugita.fd(50)
