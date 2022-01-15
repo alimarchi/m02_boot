e8ac2fdf083f7926d84c685f5e1a9fd3c7d88dd5
Alice <marchi_alice@yahoo.com>
2022-01-15 09:58:12 +0100

    objetos

--- objetos.py
+++ objetos.py
@@ -2,10 +2,16 @@ import turtle
 
 tortugita = turtle.Turtle()
 otraTortugita = turtle.Turtle()
+lentorra = turtle.Turtle()
+
+lentorra.speed(1)
 
 tortugita.shape("turtle")
+tortugita.color("blue")
 tortugita.fd(50)
+tortugita.speed(5)
 
 otraTortugita.color("green")
 otraTortugita.left(90)
 otraTortugita.fd(50)
+otraTortugita.speed(5)
